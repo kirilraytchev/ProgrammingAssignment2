99a7e09bb1408a3945f11f1c076f2c7b03ffe6b4
kirilraytchev <kiril_raytchev@hotmail.com>
2014-05-23 21:36:43 +0300

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Computing the inverse of a square matrix and using a cached result 
+## rather than performing a costly inverse computation repeatedly in case 
+## matrix has not changed.
 
-## Write a short comment describing this function
+## Creating a matrix that can cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+        m <- NULL
+        
+        ## can be set from outside environment
+        set <- function(y) 
+        {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        
+        ## can be set from outside environment
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Computing the inverse matrix returned by makeCacheMatrix
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) 
+{
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        ## Checking for a non changed matrix
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
+        
 }
